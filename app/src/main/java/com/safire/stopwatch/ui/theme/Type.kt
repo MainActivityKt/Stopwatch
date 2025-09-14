ae8b0e28fbee056fd9a386493667c4b3bafe52a1
Safire <76739907+MainActivityKt@users.noreply.github.com>
2025-09-14 20:59:19 +0430

    Used the new font

--- app/src/main/java/com/safire/stopwatch/ui/theme/Type.kt
+++ app/src/main/java/com/safire/stopwatch/ui/theme/Type.kt
@@ -2,33 +2,22 @@ package com.safire.stopwatch.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.safire.stopwatch.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+// Default Material 3 typography values
+val baseline = Typography()
+
+val figtreeFontFamily = FontFamily(Font(R.font.figtree))
+
+val AppTypography = Typography(
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = figtreeFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = figtreeFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = figtreeFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = figtreeFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = figtreeFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = figtreeFontFamily),
 )
