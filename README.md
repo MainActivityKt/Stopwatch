7a9653f6185b5f735fa2545481745e4fd00f4638
Safire <76739907+MainActivityKt@users.noreply.github.com>
2025-09-14 20:59:36 +0430

    Added the stopwatch ui and logic

---
+++ README.md
@@ -0,0 +1,31 @@
+# Stopwatach
+
+An app that measures the time elapsed between its
+activation and deactivation, built as part of my Android development internship at [Oasis Infobyte](https://oasisinfobyte.com/).
+
+
+
+## ✨ Features
+- 🎨 Beautiful UI
+- 🌙 Full support for night mode (dark theme)
+- ⚙️ Continues counting in the background as well
+- 📱 Adaptive layout
+
+
+## 🛠️ Tech Stack
+
+- App logic: Kotlin
+- Frontend / UI: Jetpack Compose
+
+## 📱 Screenshot
+
+<img src="screenshot.png" alt="doing calculation" width="300"/>
+
+
+# ©️ Credits
+
+
+| Category   | Name                    | Source       |
+|------------|-------------------------|--------------|
+| Typography | Vend Sans font          | [Google Fonts](https://fonts.google.com/specimen/Vend+Sans?query=vend+san) |
+| Drawables  | Calculator illustration | [Pixabay](https://pixabay.com/)      |
--- app/src/main/java/com/safire/stopwatch/MainActivity.kt
+++ app/src/main/java/com/safire/stopwatch/MainActivity.kt
@@ -4,14 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.safire.stopwatch.ui.theme.StopwatchTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +45,132 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StopwatchTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = {
+                        TopBar()
+                    }
+                ) { innerPadding ->
+                    App(innerPadding)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TopBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+            Text(stringResource(R.string.app_name))
+        }, colors = TopAppBarDefaults.topAppBarColors().copy(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        navigationIcon = {
+            Icon(
+                painterResource(R.drawable.ic_launcher_foreground),
+                tint = MaterialTheme.colorScheme.onPrimary,
+                contentDescription = "App icon"
+            )
+        }
     )
 }
 
+@Composable
+fun App(
+    innerPadding: PaddingValues = PaddingValues(0.dp)
+) {
+
+    var seconds by rememberSaveable { mutableIntStateOf(0) }
+    var minutes by rememberSaveable { mutableIntStateOf(0) }
+    var hours by rememberSaveable { mutableIntStateOf(0) }
+
+    var isRunning by rememberSaveable { mutableStateOf(false) }
+    var buttonText by rememberSaveable { mutableStateOf("Start") }
+
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+    ) {
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(fontSize = 50.sp, text = hours.formatTime(), fontWeight = FontWeight.SemiBold)
+            Text(fontSize = 50.sp, text = ":", fontWeight = FontWeight.SemiBold)
+            Text(fontSize = 50.sp, text = minutes.formatTime(), fontWeight = FontWeight.SemiBold)
+            Text(fontSize = 50.sp, text = ":", fontWeight = FontWeight.SemiBold)
+            Text(fontSize = 50.sp, text = seconds.formatTime(), fontWeight = FontWeight.SemiBold)
+        }
+
+        Spacer(Modifier.height(50.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(48.dp)
+        ) {
+
+            Button(
+                onClick = {
+                    isRunning = !isRunning
+                    buttonText = if (isRunning) "Pause" else "Resume"
+                },
+                modifier = Modifier
+                    .weight(0.5f)
+
+            ) {
+                Text(buttonText, style = MaterialTheme.typography.bodyLarge)
+            }
+
+            Button(
+                onClick = {
+                    isRunning = false
+                    buttonText = "Start"
+
+                    hours = 0
+                    minutes = 0
+                    seconds = 0
+
+                }, enabled = seconds > 0 || minutes > 0 || hours != 0,
+                modifier = Modifier
+                    .weight(0.5f)
+            ) {
+                Text("Reset", style = MaterialTheme.typography.bodyLarge)
+            }
+        }
+
+
+        LaunchedEffect(isRunning) {
+            while (isRunning) {
+                delay(1000L)
+                if (seconds == 59) {
+                    if (minutes == 59) {
+                        hours = hours + 1
+                        minutes = 0
+                    }
+                    seconds = 0
+                } else {
+                    seconds = seconds + 1
+                }
+
+
+            }
+        }
+    }
+}
+
+fun Int.formatTime(): String = toString().padStart(2, '0')
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     StopwatchTheme {
-        Greeting("Android")
+        App()
     }
 }
